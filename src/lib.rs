cfa3e75b74d53465ad258683d2eecabc8c7de3ff
Alexander Torstling <alexander@torstling.com>
2017-03-21 21:20:59 +0100

    more tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 extern crate iron;
 extern crate unicase;
-struct CorsMiddleware;
 
 use unicase::UniCase;
 use iron::prelude::*;
@@ -14,6 +13,7 @@ use iron::headers::{Origin,
                     AccessControlAllowMethods};
 use iron::middleware::{AroundMiddleware, Handler};
 
+struct CorsMiddleware;
 impl AroundMiddleware for CorsMiddleware {
 
     fn around(self, handler: Box<Handler>) -> Box<Handler> {
@@ -30,9 +30,14 @@ impl AroundMiddleware for CorsMiddleware {
                 // Preflight request
                 // 1.If the Origin header is not present terminate this set of steps. The request is
                 // outside the scope of this specification.
-                if req.headers.get::<Origin>().is_none() {
-                    
+                let maybe_origin = req.headers.get::<Origin>();
+                if maybe_origin.is_none() {
+                    let resp = Response::with((status::BadRequest, 
+                                               "Preflight request without Origin header"));
+                    return Ok(resp);
                 }
+                let origin = maybe_origin.unwrap();
+                let mut res = Response::with((status::NoContent));
                 //
                 // 2.If the value of the Origin header is not a case-sensitive match for any of the
                 // values in list of origins do not set any additional headers and terminate this
@@ -77,6 +82,7 @@ impl AroundMiddleware for CorsMiddleware {
                 //
                 // Otherwise, add a single Access-Control-Allow-Origin header, with either the
                 // value of the Origin header or the string "*" as value.
+                res.headers.set(AccessControlAllowOrigin::Value(format!("{}", origin)));
                 //
                 // The string "*" cannot be used for a resource that supports credentials.
                 //
@@ -87,6 +93,7 @@ impl AroundMiddleware for CorsMiddleware {
                 //
                 // Add one or more Access-Control-Allow-Methods headers consisting of (a subset of)
                 // the list of methods.
+                res.headers.set(AccessControlAllowMethods(allowed_methods));
                 //
                 // If a method is a simple method it does not need to be listed, but this is not
                 // prohibited.
@@ -99,6 +106,7 @@ impl AroundMiddleware for CorsMiddleware {
                 //
                 // Add one or more Access-Control-Allow-Headers headers consisting of (a subset of)
                 // the list of headers.
+                res.headers.set(AccessControlAllowHeaders(allowed_headers));
                 //
                 // If a header field name is a simple header and is not Content-Type, it is not
                 // required to be listed. Content-Type is to be listed as only a subset of its
@@ -106,9 +114,6 @@ impl AroundMiddleware for CorsMiddleware {
                 //
                 // Since the list of headers can be unbounded, simply returning supported headers
                 // from Access-Control-Allow-Headers can be enough.
-                let mut res = Response::with((status::NoContent));
-                res.headers.set(AccessControlAllowHeaders(allowed_headers));
-                res.headers.set(AccessControlAllowMethods(allowed_methods));
                 Ok(res)
 
             } else {
@@ -158,7 +163,18 @@ mod tests {
 	use iron::prelude::*;
 	use iron::status;
 	use self::hyper::Client;
+	use self::hyper::header::Headers;
 	use std::io::Read;
+    use iron::headers::{Origin,
+					ContentType,
+                    AccessControlRequestMethod,
+                    AccessControlAllowOrigin,
+                    AccessControlAllowHeaders,
+                    AccessControlAllowMethods};
+    use iron::middleware::{AroundMiddleware, Handler};
+    use iron::method::Method;
+    use super::CorsMiddleware;
+    use std::str::FromStr;
 
 	struct AutoServer {
 		listening: Listening,
@@ -171,7 +187,9 @@ mod tests {
 			router.get("/a",
                |_: &mut Request| Ok(Response::with((status::ImATeapot, ""))),
                "get_a");
-    		let l = Iron::new(router).http(format!("localhost:0")).unwrap();
+            let mut chain = Chain::new(router);
+            chain.link_around(CorsMiddleware);
+    		let l = Iron::new(chain).http(format!("localhost:0")).unwrap();
 			let p = l.socket.port();
 			AutoServer { listening: l, port: p }
 		}
@@ -185,12 +203,46 @@ mod tests {
 	}
 
     #[test]
-    fn options_with_requested_methods_but_no_origin_invalid() {
+    fn normal_request_possible() {
 		let server = AutoServer::new();
-		let port = server.port ;
-		println!("port is {}", port);
 		let client = Client::new();
-		let res = client.get(&format!("http://127.0.0.1:{}/a", port)).send().unwrap();
+		let res = client.get(&format!("http://127.0.0.1:{}/a", server.port)).send().unwrap();
 		assert_eq!(res.status, status::ImATeapot);
+	}
+
+    #[test]
+    fn preflight_without_origin_is_bad_request() {
+		let server = AutoServer::new();
+		let client = Client::new();
+		let mut headers = Headers::new();
+		headers.set(AccessControlRequestMethod(Method::Get));
+		let mut res = client
+				.request(Method::Options, &format!("http://127.0.0.1:{}/a", server.port))
+				.headers(headers)
+				.send().unwrap();
+		assert_eq!(res.status, status::BadRequest);
+        let mut payload = String::new();
+        res.read_to_string(&mut payload).unwrap();
+        assert_eq!(payload, "Preflight request without Origin header");
+	}
+    
+    #[test]
+    fn preflight_with_origin_accepts_same_origin() {
+		let server = AutoServer::new();
+		let client = Client::new();
+		let mut headers = Headers::new();
+		headers.set(AccessControlRequestMethod(Method::Get));
+		headers.set(Origin::from_str("http://www.a.com:8080").unwrap());
+		let mut res = client
+				.request(Method::Options, &format!("http://127.0.0.1:{}/a", server.port))
+				.headers(headers)
+				.send().unwrap();
+		assert_eq!(res.status, status::NoContent);
+        let allow_origin = res.headers.get::<AccessControlAllowOrigin>().unwrap();
+        assert_eq!(format!("{}", allow_origin) , "http://www.a.com:8080");
+        let allow_headers = res.headers.get::<AccessControlAllowHeaders>().unwrap();
+        assert_eq!(format!("{}", allow_headers) , "Content-Type, X-Requested-With");
+        let allow_methods = res.headers.get::<AccessControlAllowMethods>().unwrap();
+        assert_eq!(format!("{}", allow_methods) , "GET, PUT, POST");
 	}
 }
